99738dd4a0c4210f57db176c3f1f358adef7423f
fredmanre <fredmanre@gmail.com>
2018-03-20 15:52:46 -0400

    fix bugs and changes

--- scrappers_market.py
+++ scrappers_market.py
@@ -28,8 +28,8 @@ def game():
         divs = soup.find_all('div', class_='block-value dark-color')
         current = divs[5].text
         market_cap = None
-        dict_ = insert_into_list('GameCredits', 'GAME', markket_cap, current, resource)
-        list_json.append(game)
+        dict_ = insert_into_list('GameCredits', 'GAME', market_cap, current, resource)
+        list_json.append(dict_)
     except:
         pass
         
@@ -46,16 +46,16 @@ def enj():
         pass
     
 def sls():
-        resource = 'http://www.presstab.pw/phpexplorer/SLS/'
-        try:
-            soup = extract_with_bs(resource)
-            td = soup.find_all('td')
-            current = td[2].text
-            market_cap = td[5].text[1:]
-            dict_ = insert_into_list('SaluS', 'SLS', market_cap, current, resource)
-            list_json.append(dict_)
-        except:
-            pass
+    resource = 'http://www.presstab.pw/phpexplorer/SLS/'
+    try:
+        soup = extract_with_bs(resource)
+        td = soup.find_all('td')
+        current = td[2].text
+        market_cap = td[5].text[1:]
+        dict_ = insert_into_list('SaluS', 'SLS', market_cap, current, resource)
+        list_json.append(dict_)
+    except:
+        pass
         
         
 def sky():
@@ -64,23 +64,23 @@ def sky():
         soup = extract_with_se(resource, 2)
         span = soup.find_all('span', class_='-value')
         current = span[1].text
-        markket_cap = None
-        dict_ = insert_into_list('Skycoin', 'SKY', markket_cap, current, resource)
+        market_cap = None
+        dict_ = insert_into_list('Skycoin', 'SKY', market_cap, current, resource)
         list_json.append(dict_)
     except:
         pass
     
 
 def ubq():
-        resource = 'https://ubiqscan.io/assets/'
-        try:
-            soup = extract_with_se(resource, 2)
-            current = soup.find('td', id='ubiqAvailableSupply').text
-            market_cap = soup.find('td', id='ubiqMarketcap').text
-            dict_ = insert_into_list('Ubiq', 'UBQ', markket_cap, current, resource)
-            list_json.append(dict_)
-        except:
-            pass
+    resource = 'https://ubiqscan.io/assets/'
+    try:
+        soup = extract_with_se(resource, 2)
+        current = soup.find('td', id='ubiqAvailableSupply').text
+        market_cap = soup.find('td', id='ubiqMarketcap').text
+        dict_ = insert_into_list('Ubiq', 'UBQ', market_cap, current, resource)
+        list_json.append(dict_)
+    except:
+        pass
 
 
 def zen():
@@ -99,7 +99,7 @@ def zen():
 def xas():
     resource = 'https://explorer.asch.io/index.html'
     try:
-        soup = extract_with_bs(resource)
+        soup = extract_with_se(resource, 2)
         market_cap = soup.find('span', class_='shizhiNumber').text
         current = None
         dict_ = insert_into_list('Asch', 'XAS', market_cap, current, resource)
@@ -111,10 +111,10 @@ def xas():
 def spxtx():
     resource = 'https://www.sophiatx.com/es.html'
     try:
-        soup = extract_with_bs(resource)
+        soup = extract_with_se(resource, 2)
         current = soup.find('td', class_='stats_4').text.split(' ')[0]
         market_cap = soup.find('td', class_='stats_6').text.split(' ')[1]
-        dict_ = insert_into_list('SophiaTX', 'SPHTX', marrket_cap, current, resource)
+        dict_ = insert_into_list('SophiaTX', 'SPHTX', market_cap, current, resource)
         list_json.append(dict_)
     except:
         pass
@@ -138,25 +138,26 @@ def xby():
         soup = extract_with_se(resource, 2)
         market_cap = soup.find('span', id='market_cap_usd').text
         current = soup.find('span', id='available_supply').text
-        dict_ = insert_into_list('XTRABYTES', 'xby', market_cap, current, resource)
+        dict_ = insert_into_list('XTRABYTES', 'XBY', market_cap, current, resource)
         list_json.append(dict_)
     except:
         pass
     
 
 def smt():
-        resource = 'https://smartmesh.io/smt-token/'
-        try:
-            soup = extract_with_bs(resource)
-            p = soup.find_all('p')
-            market_cap = None
-            current = p[3].text.split(' ')[2]
-            dict_ = insert_into_list('SmartMesh', 'SMT', market_cap, current, resource)
-            list_json.append(dict_)
-        except:
-            pass
+    resource = 'https://smartmesh.io/smt-token/'
+    try:
+        soup = extract_with_bs(resource)
+        p = soup.find_all('p')
+        market_cap = None
+        current = p[3].text.split(' ')[2]
+        dict_ = insert_into_list('SmartMesh', 'SMT', market_cap, current, resource)
+        list_json.append(dict_)
+    except:
+        pass
         
         
-coins = dtr(), game(), enj(), sls(), sky(), ubq(), zen(), spxtx(), poa(), xby(), smt()
+coins = dtr(), game(), enj(), sls(), sky(), ubq(), zen(), xas(), spxtx(), poa(), xby(), smt()
 
-print(list_json)
+for l in list_json:
+    print(l, end='\n')
