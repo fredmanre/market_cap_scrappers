45b28987280202010efb74f1303b7b75a398b1fe
fredmanre <fredmanre@gmail.com>
2018-03-27 13:50:13 -0400

    scrappers en el pasado 2016-2018

--- scrappers_pasts1617.py
+++ scrappers_pasts1617.py
@@ -10,10 +10,9 @@ df = pd.DataFrame(columns=("name",
                            "marketcap_usd",
                            "price",
                            "volume(24h)",
-                           "current_supply",
                            "update_time"))
 
-sym = get_data(200)
+sym = get_data(1000)
 
 # recorremos todos los años con sus respectivos meses
 for year in enlaces:
@@ -21,11 +20,11 @@ for year in enlaces:
         enlace = enlaces[year][month]['link']
         fecha = enlaces[year][month]['date']
         # print(year, month, enlace)
-        resp = requests.get("https://coinmarketcap.com/")
+        resp = requests.get(enlace)
         html = resp.text
         soup = bs(html, "html.parser")
         # data extract from coinmarketcap
-        name = soup.find_all(class_="currency-name-container")
+        name = soup.find_all(class_="currency-name")
         market_cap = soup.find_all(class_="no-wrap market-cap text-right")
         price = soup.find_all(class_="price")
         volume_24 = soup.find_all(class_="volume")
@@ -34,33 +33,32 @@ for year in enlaces:
         # we travel coins for earch year and each month
         print(year, month)
         for i in range(100):
-            n = name[i].text
-            s = sym[n]
+            n = name[i].text.split('\n')[2]
+            s = sym.get(n, n)
             mc = market_cap[i].text.replace("\n", "").replace(" ", "").replace("$", "")
             p = price[i].text.replace("$", "")
             v = volume_24[i].text.replace("$", "").replace(',', '')
-            cs = circ_sup[i].text.split('\n')[2].replace(',', '')
-            print(n, s, mc, p, v, cs)
+            # cs = circ_sup[i].text.split('\n')[2].replace(',', '')
+            print(n, s, mc, p, v)
             # df.loc[i] = n, mc, p, v, cs, date
             # temporary dataframe to save a row data
             new_df = pd.DataFrame([
-                [n, s, mc, p, v, cs, date]],
+                [n, s, mc, p, v, date]],
                 columns=("name",
                          "symbol",
                          "marketcap_usd",
                          "price",
                          "volume(24h)",
-                         "current_supply",
                          "update_time"))
             # print the row to verify
             # print(new_df)
             # add the row in to created dataframe
             df = pd.concat([df, new_df])
 
-df.set_index('date', inplace=True)
+df.set_index('update_time', inplace=True)
 df.sort_index(inplace=True)
 print(df)
 # we convert our dataframe in csv
-df.to_csv('marketcap_past.csv', mode="w+")
-list_dict = df.read_csv('marketcap_past.csv')
+df.to_csv('marketcap_past.csv', mode="a+")
+list_dict = pd.read_csv('marketcap_past.csv')
 dict_ = list_dict.to_dict('records')
--- scrappers_pasts18.py
+++ scrappers_pasts18.py
@@ -1,15 +1,19 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import pandas as pd
-from lib.links20162018 import enlaces2
+from lib.links20162018 import enlaces_2 as enlaces
+from nametosym import get_data
 
 # dataframe empty
-df = pd.DataFrame(columns=("Name",
-                           "Market_Cap",
-                           "Price",
-                           "Volume(24h)",
-                           "Circulating_Supply",
-                           "date"))
+df = pd.DataFrame(columns=("name",
+                           "symbol",
+                           "marketcap_usd",
+                           "price",
+                           "volume(24h)",
+                           "current_supply",
+                           "update_time"))
+
+sym = get_data(1000)
 
 # recorremos todos los años con sus respectivos meses
 for year in enlaces:
@@ -17,7 +21,7 @@ for year in enlaces:
         enlace = enlaces[year][month]['link']
         fecha = enlaces[year][month]['date']
         # print(year, month, enlace)
-        resp = requests.get("https://coinmarketcap.com/")
+        resp = requests.get(enlace)
         html = resp.text
         soup = bs(html, "html.parser")
         # data extract from coinmarketcap
@@ -31,29 +35,32 @@ for year in enlaces:
         print(year, month)
         for i in range(100):
             n = name[i].text
+            s = sym.get(n, 'change')
             mc = market_cap[i].text.replace("\n", "").replace(" ", "").replace("$", "")
             p = price[i].text.replace("$", "")
             v = volume_24[i].text.replace("$", "").replace(',', '')
             cs = circ_sup[i].text.split('\n')[2].replace(',', '')
-            print(n, mc, p, v, cs)
+            print(n, s, mc, p, v, cs)
             # df.loc[i] = n, mc, p, v, cs, date
             # temporary dataframe to save a row data
             new_df = pd.DataFrame([
-                [n, mc, p, v, cs, date]],
-                columns=("Name",
-                         "Market_Cap",
-                         "Price",
-                         "Volume(24h)",
-                         "Circulating_Supply",
-                         "date"))
+                [n, s, mc, p, v, cs, date]],
+                columns=("name",
+                         "symbol",
+                         "marketcap_usd",
+                         "price",
+                         "volume(24h)",
+                         "current_supply",
+                         "update_time"))
             # print the row to verify
             # print(new_df)
             # add the row in to created dataframe
             df = pd.concat([df, new_df])
 
-df.set_index('date', inplace=True)
+df.set_index('update_time', inplace=True)
 df.sort_index(inplace=True)
 print(df)
 # we convert our dataframe in csv
-df.to_csv('marketcap_past16.csv', mode="w+")
-list_dict = df.read_csv('marketcap_past16.csv')
+df.to_csv('marketcap_past.csv', mode="w+")
+list_dict = pd.read_csv('marketcap_past.csv')
+dict_ = list_dict.to_dict('records')
