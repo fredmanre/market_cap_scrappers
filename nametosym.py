4a8e351cf5c78243c5323e12023b6ce20a4bc29b
arnaldojvg <arnaldojvg@gmail.com>
2018-03-27 10:41:11 -0400

    function names to symbols

---
+++ nametosym.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 27 10:34:10 2018
+
+@author: arnaldo
+"""
+
+import requests
+import json
+
+
+def get_data(cant):
+    r = requests.get("https://api.coinmarketcap.com/v1/ticker/?limit={}".format(cant))
+    coins = r.json()
+    names_to_sym = {}
+    for coin in coins:
+        names_to_sym[coin['name']] = coin['symbol']
+
+    return names_to_sym
+
+
+
+def main():
+    print(get_data(100))
+
+if __name__ == '__main__':
+    main()
